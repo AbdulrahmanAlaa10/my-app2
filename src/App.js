b4301b1f55bf084df8c7e8a053f145501051f4c0
alaaboody65 <alaaboody65@gmail.com>
2020-07-25 04:00:46 +0200

    create react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+
+  state = {   
+    // Initially, no file is selected 
+    selectedFile: null
+  }; 
+
+
+  onFileChange = event => {
+    // Update the state 
+    this.setState({ selectedFile: event.target.files[0] }); 
+  }; 
+
+
+  fileData = () => { 
+     
+    if (this.state.selectedFile) { 
+
+      return ( 
+        <div> 
+          <h2 className="FileDetails">File Details:</h2> 
+          <p className="phph">File Name: {this.state.selectedFile.name}</p> 
+          <p className="phph">File Type: {this.state.selectedFile.type}</p> 
+          <p className="phph"> 
+            Last Modified:{" "} {this.state.selectedFile.lastModifiedDate.toDateString()}
+          </p> 
+        </div> 
+      ); 
+    } else { 
+      return ( 
+        <div> 
+          <br /> 
+          <h4 className="befo">Choose before Pressing the Upload button</h4> 
+        </div> 
+      ); 
+    } 
+  }; 
+
+  render() {
+    return(
+       <div> 
+            <h1 className="Choose"> 
+              Choose Image  
+            </h1> 
+            <h3 className="FileUp"> 
+              File Upload! 
+            </h3> 
+            <div> 
+                <input type="file" className="ChooseFile" onChange={this.onFileChange} /> 
+            </div> 
+          {this.fileData()} 
+
+
+        <div>
+        <img src="images/16.jpg" className="image" />
+
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                 +
+              </button>
+
+              <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      ...
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        </div>
+        </div> 
+
+
+    )
+
+  };
 }
 
 export default App;
